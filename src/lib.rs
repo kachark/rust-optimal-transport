617bd1961dbb47a60a1f04373519a267ef0abd40
kachark <kkachar@hotmail.com>
2021-08-10 15:14:18 -0400

    fixed bug incorrectly initializing alpha and beta in emd_c()

--- src/lib.rs
+++ src/lib.rs
@@ -35,10 +35,10 @@ mod ffi {
 }
 
 enum ProblemType {
-    INFEASIBLE=0,
-    OPTIMAL=1,
-    UNBOUNDED=2,
-    MAX_ITER_REACHED=3
+    Infeasible=0,
+    Optimal=1,
+    Unbounded=2,
+    MaxIterReached=3
 }
 
 #[derive(Debug)]
@@ -59,7 +59,6 @@ impl Error for ProblemError {
 }
 
 
-// TODO: return ProblemType/ResultCode instead of i32
 #[allow(non_snake_case)]
 fn emd_c(a: &mut DVector<f64>, b: &mut DVector<f64>, M: &mut DMatrix<f64>, max_iter: i32)
     -> (DMatrix<f64>, f64, DVector<f64>, DVector<f64>, i32) {
@@ -69,16 +68,16 @@ fn emd_c(a: &mut DVector<f64>, b: &mut DVector<f64>, M: &mut DMatrix<f64>, max_i
     let mut result_code = 0i32;
     let _nG = 0i32;
     let mut cost = 0f64;
-    let mut alpha = dvector![0f64];
-    let mut beta = dvector![0f64];
+    let mut alpha = DVector::<f64>::zeros(n1);
+    let mut beta = DVector::<f64>::zeros(n2);
     let mut G = DMatrix::<f64>::zeros(2, 2);
     let Gv = dvector![0];
 
-    if a.len() != 0 {
+    if a.len() == 0 {
         *a = DVector::from_vec(vec![1f64; n1]).scale(1f64/n1 as f64);
     }
 
-    if b.len() != 0 {
+    if b.len() == 0 {
         *b = DVector::from_vec(vec![1f64; n2]).scale(1f64/n2 as f64);
     }
 
@@ -117,6 +116,9 @@ fn center_ot_dual(
         a_vec = a.unwrap().clone();
     }
 
+    println!("a_vec {:?}", a_vec);
+    println!("alpha0 {:?}", alpha0);
+
     if b == None {
         let nt = beta0.len();
         b_vec = DVector::from_vec(vec![1f64; nt]).scale(1f64/ (nt as f64))
@@ -124,13 +126,13 @@ fn center_ot_dual(
         b_vec = b.unwrap().clone();
     }
 
+
     let c = (b_vec.dot(beta0) - a_vec.dot(alpha0)) / (a_vec.sum() + b_vec.sum());
 
     (alpha0.add_scalar(c), beta0.add_scalar(-c))
 
 }
 
-// TODO: implement estimate_dual_null_weights()
 fn estimate_dual_null_weights(
     alpha0: &DVector<f64>, beta0: &DVector<f64>,
     a: &DVector<f64>, b: &DVector<f64>,
@@ -201,14 +203,81 @@ fn estimate_dual_null_weights(
 
 }
 
+#[allow(non_snake_case)]
+pub fn emd(a: &mut DVector<f64>, b: &mut DVector<f64>,
+       M: &mut DMatrix<f64>, num_iter_max: i32,
+       center_dual: bool) -> DMatrix<f64> {
+
+    let (m0, m1) = M.shape();
+
+    // Uniform distribution
+    if a.len() == 0 {
+        *a = DVector::from_vec(vec![1f64; m0]).scale(1f64/m0 as f64);
+    }
+
+    if b.len() == 0 {
+        *b = DVector::from_vec(vec![1f64; m1]).scale(1f64/m1 as f64);
+    }
+
+    // Check dimensions
+    assert_eq!(a.len(), m0, "Dimension mismatch check dimensions of M with a");
+    assert_eq!(b.len(), m1, "Dimension mismatch check dimensions of M with b");
+
+    // Ensure the same mass
+    assert_eq!(a.sum(), b.sum(), "a and b vector must have the same sum");
+
+    // b = b * a.sum/b.sum
+    b.scale_mut(a.sum()/b.sum());
+
+    // not_asel == ~asel, not_bsel == ~bsel
+    // binary indexing of non-zero weights
+    let mut not_asel = DVector::<i32>::zeros(a.len());
+    for (i, val) in a.iter().enumerate() {
+        if *val == 0f64 {
+            not_asel[i] = 0;
+        } else {
+            not_asel[i] = 1;
+        }
+    }
+
+    let mut not_bsel = DVector::<i32>::zeros(b.len());
+    for (i, val) in b.iter().enumerate() {
+        if *val == 0f64 {
+            not_bsel[i] = 1;
+        } else {
+            not_bsel[i] = 0;
+        }
+    }
+
+    let (G, _cost, mut u, mut v, result_code) = emd_c(a, b, M, num_iter_max);
+
+    if center_dual {
+        let result = center_ot_dual(&u, &v, Some(a), Some(b));
+        u = result.0;
+        v = result.1;
+    }
+
+    if not_asel.sum() > 1 || not_bsel.sum() > 1 {
+        let result = estimate_dual_null_weights(&u, &v, a, b, M);
+        u = result.0;
+        v = result.1;
+    }
+
+    match check_result(result_code) {
+        Err(error) => panic!("{:?}", error),
+        Ok(_) => G
+    }
+
+}
+
 
 fn check_result(result_code: i32) -> Result<(), ProblemError> {
 
-    if result_code == ProblemType::OPTIMAL as i32 {
+    if result_code == ProblemType::Optimal as i32 {
         Ok(())
-    } else if result_code == ProblemType::UNBOUNDED as i32 {
+    } else if result_code == ProblemType::Unbounded as i32 {
         Err( ProblemError{details: String::from("Problem unbounded")} )
-    } else if result_code == ProblemType::MAX_ITER_REACHED as i32 {
+    } else if result_code == ProblemType::MaxIterReached as i32 {
         Err( ProblemError{details: String::from("numItermax reached before optimality. Try to increase numItermax")} )
     } else {
         Err( ProblemError{details: String::from("Problem infeasible. Check that a and b are in the simplex")} )
@@ -220,8 +289,7 @@ fn check_result(result_code: i32) -> Result<(), ProblemError> {
 #[cfg(test)]
 mod tests {
 
-    use na::{dvector, DVector, DMatrix};
-    use crate::ffi::*;
+    use na::{dvector, DMatrix};
 
     #[test]
     fn it_works() {
@@ -237,9 +305,25 @@ mod tests {
                                 &[0.0, 1.0,
                                 1.0, 0.0]);
 
-        let (G, cost, u, v, result_code) = super::emd_c(&mut a, &mut b, &mut M, 10000);
+        let (G, _cost, _u, _v, _result_code) = super::emd_c(&mut a, &mut b, &mut M, 10000);
 
         println!("{:?}", G);
 
     }
+
+    #[test]
+    fn test_emd() {
+
+        let mut a = dvector![0.5, 0.5];
+        let mut b = dvector![0.5, 0.5];
+        let mut M = DMatrix::<f64>::from_row_slice(2, 2,
+                                &[0.0, 1.0,
+                                1.0, 0.0]);
+
+        let gamma = super::emd(&mut a, &mut b, &mut M, 100000, true);
+
+        println!("{:?}", gamma);
+
+    }
+
 }
