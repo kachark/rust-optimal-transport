11ca032e81477afc7cef77454f19ac33ea7b2274
kachark <kkachar@hotmail.com>
2021-08-15 23:09:57 -0400

    Fixed bug with euclidean_distances() np.einsum() implementation

--- src/utils.rs
+++ src/utils.rs
@@ -35,34 +35,37 @@ fn euclidean_distances(x: &DMatrix<f64>, y: &DMatrix<f64>, squared: bool) -> DMa
     // - element-wise multiplication
     // ommitted j in output : sum along j axis
     // - summation in j
-    let a2 = x.component_mul(x).row_sum();
+    let a2 = x.component_mul(x).column_sum();
     // einsum('ij,ij->i', Y, Y)
-    let b2 = y.component_mul(y).row_sum();
+    let b2 = y.component_mul(y).column_sum();
 
     // for y = 3x3 mtx of 5.0, b2 = [75, 75, 75]
 
-    println!("b2: {:?}", b2);
+    // println!("a2: {:?}", a2);
+    // println!("b2: {:?}", b2);
 
-    let tmpscalar = -2f64 * x.dot(&y.transpose());
-    let mut tmp = DMatrix::<f64>::zeros(a2.len(), b2.len());
+    // let tmpscalar = -2f64 * x.dot(&y.transpose());
+    // let mut tmp = DMatrix::<f64>::zeros(a2.len(), b2.len());
+    let mut c = (x * &y.transpose()).scale(-2f64);
 
-    for (i, valx) in a2.iter().enumerate() {
-        for (j, valy) in b2.iter().enumerate() {
+    // c += a2[:, None]
+    for (mut row, a2val) in c.row_iter_mut().zip(&a2.transpose()) {
+        for ele in row.iter_mut() {
+            *ele += a2val;
+        }
+    }
 
-            tmp[(i,j)] = valx + valy + tmpscalar;
-
+    // c += b2[None, :]
+    for (mut col, b2val) in c.column_iter_mut().zip(&b2) {
+        for ele in col.iter_mut() {
+            *ele += b2val;
         }
     }
 
     // c = nx.maximum(c, 0)
-    let mut c = DMatrix::<f64>::zeros(a2.len(), b2.len());
-    for (i, row) in tmp.row_iter().enumerate() {
-        for (j, val) in row.iter().enumerate() {
-            if *val > 0f64 {
-                c[(i,j)] = *val;
-            } else {
-                c[(i,j)] = 0f64;
-            }
+    for val in c.iter_mut() {
+        if *val <= 0f64 {
+            *val = 0f64;
         }
     }
 
@@ -98,15 +101,23 @@ mod tests {
     #[test]
     fn test_euclidean_distances() {
 
-        let x = DMatrix::<f64>::zeros(3,3);
-        let y = DMatrix::from_element(3, 3, 5.0);
+        let x = DMatrix::<f64>::zeros(3, 5);
+        let y = DMatrix::from_row_slice(3, 5, vec![5.0; 15].as_slice());
+        // let y = DMatrix::from_element(3, 5, 5.0);
 
-        println!("{:?}", super::euclidean_distances(&x, &y, false));
+        println!("euclidean_distances: {:?}", super::euclidean_distances(&x, &y, false));
 
-        let truth = DMatrix::from_row_slice(3,3,
-                    &[75.0, 75.0, 75.0,
-                    75.0, 75.0, 75.0,
-                    75.0, 75.0, 75.0]);
+        // squared = true
+        // let truth = DMatrix::from_row_slice(3,3,
+        //             &[125.0, 125.0, 125.0,
+        //             125.0, 125.0, 125.0,
+        //             125.0, 125.0, 125.0]);
+
+        // squared = false
+        let _truth = DMatrix::from_row_slice(3,3,
+                    &[11.180339887498949, 11.180339887498949, 11.180339887498949,
+                    11.180339887498949, 11.180339887498949, 11.180339887498949,
+                    11.180339887498949, 11.180339887498949, 11.180339887498949]);
 
     }
 
@@ -116,9 +127,9 @@ mod tests {
         let x = DMatrix::<f64>::zeros(3,3);
         let y = DMatrix::from_element(3, 3, 5.0);
 
-        println!("{:?}", super::dist(&x, Some(&y), super::MetricType::SqEuclidean));
+        println!("dist: {:?}", super::dist(&x, Some(&y), super::MetricType::SqEuclidean));
 
-        let truth = DMatrix::from_row_slice(3,3,
+        let _truth = DMatrix::from_row_slice(3,3,
                     &[8.660254037844387, 8.660254037844387, 8.660254037844387,
                     8.660254037844387, 8.660254037844387, 8.660254037844387,
                     8.660254037844387, 8.660254037844387, 8.660254037844387]);
