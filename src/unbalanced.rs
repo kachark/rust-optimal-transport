662f638c8c4e52811d7ad766a89a691132e70d2c
kachark <kkachar@hotmail.com>
2021-08-24 12:59:07 -0400

    initial implementation of sinkhorn_knopp_unbalanced

--- src/unbalanced.rs
+++ src/unbalanced.rs
@@ -1,7 +1,7 @@
 
 pub mod unbalanced {
 
-use na::{DVector, DMatrix};
+use na::{DVector, dvector, DMatrix};
 
 pub enum UnbalancedSolverType {
     Sinkhorn,
@@ -76,16 +76,138 @@ fn sinkhorn_knopp_unbalanced(
 
     // if a and b empty, default to uniform distribution
     if a.len() == 0 {
-        *a = DVector::from_vec(vec![1f64; dim_a]).scale(1f64/dim_a as f64);
+        *a = DVector::from_vec(vec![1f64 / (dim_a as f64); dim_a]);
     }
 
     if b.len() == 0 {
-        // TODO: this may have to be row-major
-        *b = DMatrix::from_vec(1, dim_b, vec![1f64; dim_b]).scale(1f64/dim_b as f64);
+        // ensure row-major
+        *b = DMatrix::from_row_slice(1, dim_b, vec![1f64 / (dim_b as f64); dim_b].as_slice());
     }
 
+    let n_hists = b.shape().1;
 
-    DMatrix::<f64>::zeros(2,2)
+    // we assume that no distances are null except those of the diagonal distances
+    let mut u = DVector::<f64>::from_vec(vec![1f64 / (dim_a as f64); dim_a]);
+    let mut v = DMatrix::<f64>::from_row_slice(dim_b, n_hists, vec![1f64 / (dim_b as f64); dim_b].as_slice());
+
+    // K = exp(-M/reg)
+    let mut k = M.clone();
+    for m in k.iter_mut() {
+        *m = (*m/-reg).exp();
+    }
+
+    let fi = reg_m / (reg_m + reg);
+    let err = 1f64;
+
+
+    let mut iterations = 100000;
+    if let Some(val) = num_iter_max {
+        iterations = val;
+    }
+
+    let mut stop = 1E-6;
+    if let Some(val) = stop_threshold {
+        stop = val;
+    }
+
+    for i in 0..iterations {
+
+        let uprev = u.clone();
+        let vprev = v.clone();
+
+        let kv = &k * &v; // good
+
+        if i == 0 {
+            println!("{:?}", &kv);
+        }
+
+        // u = (a/kv) ** fi
+        for (i, ele_u) in u.iter_mut().enumerate() {
+            *ele_u = (a[i] / kv[i]).powf(fi);
+        } // good
+        if i == 0 {
+            println!("{:?}", &u);
+        }
+
+        let ktu = &k.transpose() * &u; // good
+        if i == 0 {
+            println!("{:?}", &ktu);
+        }
+        // v = (b/ktu) ** fi
+        for (i, ele_v) in v.iter_mut().enumerate() {
+            *ele_v = (b[i] / ktu[i]).powf(fi);
+        } // good
+
+        if i == 0 {
+            println!("{:?}", &v);
+        }
+
+        // check for machine precision
+        let mut ktu_0_flag = false;
+        let mut u_nan_flag = false;
+        let mut u_inf_flag = false;
+        let mut v_nan_flag = false;
+        let mut v_inf_flag = false;
+
+        for ele in ktu.iter() {
+            if *ele == 0f64 {
+                ktu_0_flag = true;
+            }
+        }
+
+        for ele in u.iter() {
+            if (*ele).is_nan() {
+                u_nan_flag = true;
+            }
+
+            if (*ele).is_infinite() {
+                u_inf_flag = true;
+            }
+        }
+
+        for ele in v.iter() {
+            if (*ele).is_nan() {
+                v_nan_flag = true;
+            }
+
+            if (*ele).is_infinite() {
+                v_inf_flag = true;
+            }
+        }
+
+        // Check stop conditions
+        if ktu_0_flag == true || u_nan_flag == true || u_inf_flag == true
+            || v_nan_flag == true || v_inf_flag == true {
+            u = uprev;
+            v = vprev;
+            break;
+        }
+
+        // let err_u = abs(u-uprev).max() / max(abs(u).max(), abs(uprev).max(), 1.)
+        // let err_v = abs(v-vprev).max() / max(abs(v).max(), abs(vprev).max(), 1.)
+        let err_u = (&u-&uprev).amax() / (dvector![u.amax(), uprev.amax(), 1f64].max());
+        let err_v = (&v-&vprev).amax() / (dvector![v.amax(), vprev.amax(), 1f64].max());
+        let err = 0.5 * (err_u + err_v);
+        if err < stop {
+            break;
+        }
+
+
+        // res = np.einsum('ik,ij,jk,ij->k', u, K, v, M);
+        // einsum('ij,ij->i', Y, Y)
+        // let b2 = y.component_mul(y).column_sum();
+
+    }
+
+    // let mut result = DMatrix::<f64>::zeros(k.shape().0, k.shape().1);
+    let mut result = k.clone();
+    for (i, mut row) in result.row_iter_mut().enumerate() {
+        for (j, k) in row.iter_mut().enumerate() {
+            *k *= u[i] * v[j];
+        }
+    }
+
+    result
 
 }
 
@@ -108,5 +230,58 @@ fn sinkhorn_stabilized_unbalanced(
     DMatrix::<f64>::zeros(2,2)
 
 }
+
+} // mod unbalanced
+
+
+#[cfg(test)]
+mod tests {
+
+    use na::{DVector, DMatrix};
+
+    #[test]
+    fn test_sinkhorn_unbalanced() {
+
+        let mut a = DVector::from_vec(vec![1./3., 1./3., 1./3.]);
+        let mut b = DMatrix::from_vec(3, 1, vec![1./3., 1./3., 1./3.]);
+        let reg = 2.0;
+        let reg_m = 3.0;
+        let mut m = DMatrix::<f64>::from_row_slice(3, 3, &[0.5, 0.0, 0.0, 0.0, 0.5, 0.0, 0.0, 0.0, 0.5]);
+
+        let result = super::unbalanced::sinkhorn_unbalanced(&mut a, &mut b, &mut m,
+                                                            reg, reg_m, super::unbalanced::UnbalancedSolverType::Sinkhorn,
+                                                            None, None, None);
+
+        // println!("{:?}", result);
+
+        // squared = false
+        let truth = DMatrix::from_row_slice(3,3,
+                    &[0.15874225, 0.20382908, 0.20382908,
+                    0.20382908, 0.15874225, 0.20382908,
+                    0.20382908, 0.20382908, 0.15874225]);
+
+        assert!(result.relative_eq(&truth, 1E-6, 1E-2));
+
+    }
+
+//     #[test]
+//     fn test_sinkhorn_knopp() {
+
+//         let mut a = DVector::from_vec(vec![1./3., 1./3., 1./3.]);
+//         let mut b = DMatrix::from_vec(3, 1, vec![1./3., 1./3., 1./3.]);
+//         let mut u = DVector::from_vec(vec![1./3., 1./3., 1./3.]);
+//         let mut v = DVector::from_vec(vec![1./3., 1./3., 1./3.]);
+//         let reg = 2.0;
+//         let reg_m = 3.0;
+//         let mut m = DMatrix::<f64>::from_row_slice(3, 3, &[0.5, 0.0, 0.0, 0.0, 0.5, 0.0, 0.0, 0.0, 0.5]);
+
+//         let result = super::sinkhorn_knopp_unbalanced(&mut a, &mut b, &mut m,
+//                                                     reg, reg_m, super::unbalanced::UnbalancedSolverType::Sinkhorn,
+//                                                     None, None, None);
+
+//         println!("{:?}", result);
+
+
+//     }
 
 }
